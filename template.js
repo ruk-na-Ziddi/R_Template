b17b9cff1b0b341fa9edb0f7cab5047add4a181a
Ankur Kumar Singh <aksinghmt@gmail.com>
2016-07-25 10:51:25 +0530

    t.Tempalate is a constructor has apply property | applyTemplate applies the template if condition matches

--- template.js
+++ template.js
@@ -1,2 +1,20 @@
 var t = {};
 exports.t = t;
+
+var applyTemplate = function(sentence, bag){
+	var keys = Object.keys(bag);
+
+	return sentence.split("").map(function(word){
+		return (keys.indexOf(word) > -1) ? bag[word] : word;
+	}).join("");
+}
+
+t.Template = function(sentence){
+	this.sentence = sentence;
+
+	Object.defineProperty(this, "apply", {
+		"value" : function(bag){
+			return applyTemplate(this.sentence, bag)
+		}
+	})
+}
